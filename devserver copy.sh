a7fedcc608bbef4dea40fc0bcd6b63cee0b27aa3
SMC Netwolf <139656570+SMCNetwolf@users.noreply.github.com>
2025-04-27 09:05:34 -0300

    Delete devserver copy.sh

--- devserver copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source .venv/bin/activate
-# Set the PORT environment variable
-export PORT=8080
-# Set the FLASK_APP environment variable
-export FLASK_APP=app
-# Run flask in debug mode, using the specified port.
-flask run --debug --port=$PORT
